feff455087d402a7068719d784737fdaa40204e2
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-12-04 21:52:49 +0200

    update the UI

--- app/src/main/java/com/nicos/navigation_3_compose/screens/ScreenA.kt
+++ app/src/main/java/com/nicos/navigation_3_compose/screens/ScreenA.kt
@@ -1,26 +1,36 @@
 package com.nicos.navigation_3_compose.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.nicos.navigation_3_compose.navigation.screen_routes.ScreenC
 import com.nicos.navigation_3_compose.navigation.screen_routes.navigation_3.Navigator
 
 @Composable
 fun ScreenA(navigator: Navigator) {
     Scaffold { paddingValues ->
-        Text(
-            text = "Screen A",
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-                .wrapContentSize(Alignment.Center)
-        )
+        Column {
+            Text(
+                text = "Screen A",
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .wrapContentSize(Alignment.Center)
+            )
+            Button(onClick = {
+                navigator.navigate(ScreenC)
+            }) {
+                Text(text = "Navigate to Screen C")
+            }
+        }
     }
 }
--- app/src/main/java/com/nicos/navigation_3_compose/screens/ScreenB.kt
+++ app/src/main/java/com/nicos/navigation_3_compose/screens/ScreenB.kt
@@ -1,26 +1,37 @@
 package com.nicos.navigation_3_compose.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.nicos.navigation_3_compose.navigation.screen_routes.ScreenC
+import com.nicos.navigation_3_compose.navigation.screen_routes.ScreenD
 import com.nicos.navigation_3_compose.navigation.screen_routes.navigation_3.Navigator
 
 @Composable
 fun ScreenB(navigator: Navigator) {
     Scaffold { paddingValues ->
-        Text(
-            text = "Screen B",
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-                .wrapContentSize(Alignment.Center)
-        )
+        Column {
+            Text(
+                text = "Screen B",
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .wrapContentSize(Alignment.Center)
+            )
+            Button(onClick = {
+                navigator.navigate(ScreenD)
+            }) {
+                Text(text = "Navigate to Screen D")
+            }
+        }
     }
 }
