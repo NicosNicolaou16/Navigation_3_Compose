27909a4bf4d43b7be03e071ed5ae9c87840d8816
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-12-05 02:00:54 +0200

    add the animation for specific screen and whole app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.navigation3)
     implementation(libs.androidx.material3.adaptive.navigation3)
     implementation(libs.kotlinx.serialization.core)
+    implementation(libs.androidx.compose.material3.adaptive)
+    // Test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
