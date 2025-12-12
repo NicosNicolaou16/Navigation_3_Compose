d6020371f0b5bea0eaaa4802fb7582d040d483a3
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-12-13 00:03:14 +0200

    improve the nav3

--- app/src/main/java/com/nicos/navigation_3_compose/navigation/screen_routes/navigation_3/NavCompose.kt
+++ app/src/main/java/com/nicos/navigation_3_compose/navigation/screen_routes/navigation_3/NavCompose.kt
@@ -1,15 +1,12 @@
 package com.nicos.navigation_3_compose.navigation.screen_routes.navigation_3
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.rememberNavBackStack
 
 @Composable
 fun NavKey.navigationState(): NavigationState {
-    return remember {
-        NavigationState(
-            backStacks = mutableStateListOf<NavKey>(this)
-        )
-    }
+    return NavigationState(
+        backStacks = rememberNavBackStack(this)
+    )
 }
