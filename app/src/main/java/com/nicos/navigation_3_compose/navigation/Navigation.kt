85f178e5e320a061191c46a012e0183424523847
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-04-11 00:40:26 +0300

    migrate the scenes strategy base on version 1.1.0

--- app/src/main/java/com/nicos/navigation_3_compose/navigation/Navigation.kt
+++ app/src/main/java/com/nicos/navigation_3_compose/navigation/Navigation.kt
@@ -15,7 +15,6 @@ import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.material3.adaptive.navigation3.ListDetailSceneStrategy
 import androidx.compose.material3.adaptive.navigation3.rememberListDetailSceneStrategy
 import androidx.compose.runtime.Composable
-import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.ui.NavDisplay
